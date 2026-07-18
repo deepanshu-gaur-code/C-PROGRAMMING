a8da604ea07bb40c3a0f6286615f0229a9193ddc
Deepanshu gaur <dgaur7204@gmail.com>
2026-07-18 18:56:06 +0530

    Initial commit: Upload C language ch - 01 & practice programs
    My C programming learning journey with practice programs, exercises, and projects using GCC and Visual Studio Code.

---
+++ 07_escape_sequence.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    char ch = 'a';
+    ch = 'c';
+    ch = '\n'; // represent a new line
+    printf("Hey I am good \n nice"); // \n for new line and \t for some more space betwwn words and \" is for " and \' is for ' and \\ is for \ 
+    return 0;
+}
