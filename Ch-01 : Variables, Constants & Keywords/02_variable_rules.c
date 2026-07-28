3ff96632eb8a76ceccca4fd5861cce88a12eaace
Deepanshu gaur <dgaur7204@gmail.com>
2026-07-28 18:01:47 +0530

    Uploaded Chapter 1 programs

---
+++ Ch-01 : Variables, Constants & Keywords/02_variable_rules.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int cab;
+    int harry;
+    int Harry;
+    int harry_good;
+    return 0;
+}
---
+++ Ch-01 : Variables, Constants & Keywords/05_comments.c
@@ -0,0 +1,14 @@
+ /*
+Program: 05_comments.c
+Author: Deepanshu
+Date: 29-06-2026
+*/
+#include<stdio.h>
+
+int main(){
+   
+    // I am writing this just for fun.
+    printf("Hello world");
+    // Return 0 is returning 0 to the OS
+    return 0; // This is return
+}
